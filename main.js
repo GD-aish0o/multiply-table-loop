67367f87c5cc172dbcb5e03a3555ed3bd95656f7
Aqsaa Qaazi <159919683+GD-aish0o@users.noreply.github.com>
2024-05-16 02:46:32 +0500

    added 2 files.
    these files contain code of a "for loop" which prints the multiplication table of 5 / or any other number
     are .ts (typescript) & .js (javascript) that work in node.js environment only.

---
+++ main.js
@@ -0,0 +1,6 @@
+// Write a for loop to print the multiplication table for the number 5.
+var table = 5;
+var count = 12;
+for (var i = 1; i <= count; i++) {
+    console.log("".concat(table, " X ").concat(i, " = ").concat(table * i));
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Write a for loop to print the multiplication table for the number 5.
+// for experiment you can change the number of const table.
+const table = 5;
+const count = 12;
+
+for (let i = 1; i <= count; i++) {
+    console.log(`${table} X ${i} = ${table * i}`);
+}
